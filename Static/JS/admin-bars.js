a5e55edbbb0b0bbbb0c0b5cc0360339cd77d4a76
SpaceWaffle42 <86594101+SpaceWaffle42@users.noreply.github.com>
2024-12-06 14:45:23 +0000

    Apex text colour to white

--- Static/JS/admin-bars.js
+++ Static/JS/admin-bars.js
@@ -1,56 +1,58 @@
 
-      
-        var options = {
-            series: [{
-            name: 'Net Profit',
-            data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
-          }, {
-            name: 'Revenue',
-            data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
-          }, {
-            name: 'Free Cash Flow',
-            data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
-          }],
-            chart: {
-            type: 'bar',
-            height: 350
-          },
-          plotOptions: {
-            bar: {
-              horizontal: false,
-              columnWidth: '55%',
-              borderRadius: 5,
-              borderRadiusApplication: 'end'
-            },
-          },
-          dataLabels: {
-            enabled: false
-          },
-          stroke: {
-            show: true,
-            width: 2,
-            colors: ['transparent']
-          },
-          xaxis: {
-            categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
-          },
-          yaxis: {
-            title: {
-              text: '$ (thousands)'
-            }
-          },
-          fill: {
-            opacity: 1
-          },
-          tooltip: {
-            y: {
-              formatter: function (val) {
-                return "$ " + val + " thousands"
-              }
-            }
-          }
-          };
-  
-          var barchart = new ApexCharts(document.querySelector("#BarGraph"), options);
-          barchart.render();
-        
+
+var options = {
+  series: [{
+    name: 'Net Profit',
+    data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+  }, {
+    name: 'Revenue',
+    data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+  }, {
+    name: 'Free Cash Flow',
+    data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+  }],
+  chart: {
+    type: 'bar',
+    height: 350
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      columnWidth: '55%',
+      borderRadius: 5,
+      borderRadiusApplication: 'end'
+    },
+  },
+  dataLabels: {
+    enabled: false
+  },
+  theme: {
+    mode: 'dark',
+  },
+  stroke: {
+    show: true,
+    width: 2,
+    colors: ['transparent']
+  },
+  xaxis: {
+    categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+  },
+  yaxis: {
+    title: {
+      text: '$ (thousands)'
+    }
+  },
+  fill: {
+    opacity: 1
+  },
+  tooltip: {
+    y: {
+      formatter: function (val) {
+        return "$ " + val + " thousands"
+      }
+    }
+  }
+};
+
+var barchart = new ApexCharts(document.querySelector("#BarGraph"), options);
+barchart.render();
--- Static/JS/scores-charts.js
+++ Static/JS/scores-charts.js
@@ -1,118 +1,127 @@
 var options = {
-    chart: {
-      height: 560,
-      type: "area"
-    },
-    dataLabels: {
-      enabled: false
-    },
-    series: [
-      {
-        name: "Series 1",
-        data: [45, 52, 38, 45, 19, 23, 2]
-      }
-    ],
-    fill: {
-      type: "gradient",
-      gradient: {
-        shadeIntensity: 1,
-        opacityFrom: 0.7,
-        opacityTo: 0.9,
-        stops: [0, 90, 100]
-      }
-    },
-    xaxis: {
-      categories: [
-        "01 Jan",
-        "02 Jan",
-        "03 Jan",
-        "04 Jan",
-        "05 Jan",
-        "06 Jan",
-        "07 Jan"
-      ]
+  chart: {
+    height: 560,
+    type: "area"
+  },
+  dataLabels: {
+    enabled: false
+  },
+  theme: {
+    mode: 'dark',
+  },
+  series: [
+    {
+      name: "Series 1",
+      data: [45, 52, 38, 45, 19, 23, 2]
     }
-  };
-  
-  var chart = new ApexCharts(document.querySelector("#ChartCat1"), options);
-
-  var options2 = {
-    chart: {
-      height: 560,
-      type: "area"
-    },
-    dataLabels: {
-      enabled: false
-    },
-    series: [
-      {
-        name: "Series 1",
-        data: [45, 52, 38, 45, 19, 23, 2]
-      }
-    ],
-    fill: {
-      type: "gradient",
-      gradient: {
-        shadeIntensity: 1,
-        opacityFrom: 0.7,
-        opacityTo: 0.9,
-        stops: [0, 90, 100]
-      }
-    },
-    xaxis: {
-      categories: [
-        "01 Jan",
-        "02 Jan",
-        "03 Jan",
-        "04 Jan",
-        "05 Jan",
-        "06 Jan",
-        "07 Jan"
-      ]
+  ],
+  fill: {
+    type: "gradient",
+    gradient: {
+      shadeIntensity: 1,
+      opacityFrom: 0.7,
+      opacityTo: 0.9,
+      stops: [0, 90, 100]
     }
-  };
-  
-  var chart2 = new ApexCharts(document.querySelector("#ChartCat2"), options);
-  
-  var options3 = {
-    chart: {
-      height: 560,
-      type: "area"
-    },
-    dataLabels: {
-      enabled: false
-    },
-    series: [
-      {
-        name: "Series 1",
-        data: [45, 52, 38, 45, 19, 23, 2]
-      }
-    ],
-    fill: {
-      type: "gradient",
-      gradient: {
-        shadeIntensity: 1,
-        opacityFrom: 0.7,
-        opacityTo: 0.9,
-        stops: [0, 90, 100]
-      }
-    },
-    xaxis: {
-      categories: [
-        "01 Jan",
-        "02 Jan",
-        "03 Jan",
-        "04 Jan",
-        "05 Jan",
-        "06 Jan",
-        "07 Jan"
-      ]
+  },
+  xaxis: {
+    categories: [
+      "01 Jan",
+      "02 Jan",
+      "03 Jan",
+      "04 Jan",
+      "05 Jan",
+      "06 Jan",
+      "07 Jan"
+    ]
+  }
+};
+
+var chart = new ApexCharts(document.querySelector("#ChartCat1"), options);
+
+var options2 = {
+  chart: {
+    height: 560,
+    type: "area"
+  },
+  dataLabels: {
+    enabled: false
+  },
+  theme: {
+    mode: 'dark',
+  },
+  series: [
+    {
+      name: "Series 1",
+      data: [45, 52, 38, 45, 19, 23, 2]
     }
-  };
-  
-  var ChartTotal = new ApexCharts(document.querySelector("#ChartTotal"), options);
+  ],
+  fill: {
+    type: "gradient",
+    gradient: {
+      shadeIntensity: 1,
+      opacityFrom: 0.7,
+      opacityTo: 0.9,
+      stops: [0, 90, 100]
+    }
+  },
+  xaxis: {
+    categories: [
+      "01 Jan",
+      "02 Jan",
+      "03 Jan",
+      "04 Jan",
+      "05 Jan",
+      "06 Jan",
+      "07 Jan"
+    ]
+  }
+};
+
+var chart2 = new ApexCharts(document.querySelector("#ChartCat2"), options);
+
+var options3 = {
+  chart: {
+    height: 560,
+    type: "area"
+  },
+  dataLabels: {
+    enabled: false
+  },
+  theme: {
+    mode: 'dark',
+  },
+  series: [
+    {
+      name: "Series 1",
+      data: [45, 52, 38, 45, 19, 23, 2]
+    }
+  ],
+  fill: {
+    type: "gradient",
+    gradient: {
+      shadeIntensity: 1,
+      opacityFrom: 0.7,
+      opacityTo: 0.9,
+      stops: [0, 90, 100]
+    }
+  },
+  xaxis: {
+    categories: [
+      "01 Jan",
+      "02 Jan",
+      "03 Jan",
+      "04 Jan",
+      "05 Jan",
+      "06 Jan",
+      "07 Jan"
+    ]
+  }
+};
+
+var ChartTotal = new ApexCharts(document.querySelector("#ChartTotal"), options);
 
 
-  chart2.render();
-  chart.render();
-  ChartTotal.render();
+chart2.render();
+chart.render();
+ChartTotal.render();
