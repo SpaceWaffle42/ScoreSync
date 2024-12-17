3e4195c077455b3538daa5dae3f8dc03c729fc45
SpaceWaffle42 <86594101+SpaceWaffle42@users.noreply.github.com>
2024-12-17 03:40:00 +0000

    initial DB work

--- app.py
+++ app.py
@@ -2,36 +2,91 @@ from flask import Flask, render_template, request, redirect, url_for, make_respo
 import pathlib
 import os
 import datetime
+import sqlite3
 
 app = Flask(__name__)
 
 py_path = pathlib.Path(__file__).parent.resolve()
 db_path = os.path.join(py_path, "database.db")
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     return render_template('index.html')
 
-@app.route('/index')
+@app.route('/index', methods=["GET", "POST"])
 def home():
     return render_template('index.html')
 
-@app.route('/nav')
+@app.route('/nav', methods=["GET", "POST"])
 def nav():
     return render_template('nav.html')
 
-@app.route('/footer')
+@app.route('/footer', methods=["GET", "POST"])
 def footer():
     return render_template('footer.html')
 
-@app.route('/admin')
+@app.route('/admin', methods=["GET", "POST"])
 def admin():
     return render_template('admin.html')
 
-@app.route('/scoreboard')
-def scoreboard():
-    return render_template('scoreboard.html')
 
+@app.route("/stands")
+def get_stands():
+    con = sqlite3.connect(os.path.join(py_path, "database.db"))
+    cur = con.cursor()
+
+    # Simplified SQL query to select all data
+    sql = "SELECT * FROM stands ORDER BY host;"
+    cur.execute(sql)
+
+    dbfilter = [
+        {
+
+        }
+        for row in cur.fetchall()
+    ]
+
+    con.close()
+    return jsonify(dbfilter)
+
+@app.route("/teams")
+def get_teams():
+    con = sqlite3.connect(os.path.join(py_path, "database.db"))
+    cur = con.cursor()
+
+    # Simplified SQL query to select all data
+    sql = "SELECT * FROM teams ORDER BY host;"
+    cur.execute(sql)
+
+    dbfilter = [
+        {
+
+        }
+        for row in cur.fetchall()
+    ]
+
+    con.close()
+    return jsonify(dbfilter)
+
+
+@app.route("/data")
+def get_data():
+    con = sqlite3.connect(os.path.join(py_path, "database.db"))
+    cur = con.cursor()
+
+    # Simplified SQL query to select all data
+    sql = "SELECT * FROM data ORDER BY host;"
+    cur.execute(sql)
+
+    dbfilter = [
+        {
+
+        }
+        for row in cur.fetchall()
+    ]
+
+    con.close()
+    return jsonify(dbfilter)
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True, host='0.0.0.0', port=5000)
---
+++ database.py
@@ -0,0 +1,151 @@
+import sqlite3
+import pathlib
+import os
+import json
+
+py_path = pathlib.Path(__file__).parent.resolve()
+
+class Database():
+    def initial():
+        con = sqlite3.connect(os.path.join(py_path, "database.db"))
+        cur = con.cursor()
+
+        sql = "SELECT name FROM sqlite_master WHERE type='table' AND name= (?)"
+
+        listOfTables = cur.execute(sql, ("data",)).fetchall()
+        if listOfTables == []:
+            print("Table not found!")
+            tabledate = '''
+            CREATE TABLE "data" (
+                team TEXT PRIMARY KEY,
+                points TEXT,
+                stand TEXT,
+                date TEXT
+            );
+            '''
+            cur.execute(tabledate)
+
+            tabledate = '''
+            CREATE TABLE "stands" (
+                stand TEXT PRIMARY KEY,
+                code INT
+            );
+            '''
+            cur.execute(tabledate)
+
+            tabledate = '''
+            CREATE TABLE "teams" (
+                team TEXT PRIMARY KEY
+            );
+            '''
+            cur.execute(tabledate)
+            con.close()
+        else:
+            pass
+
+    # def data():
+    #     con = sqlite3.connect(os.path.join(py_path, "database.db"))
+    #     cur = con.cursor()
+    #     sql = "SELECT * FROM data ORDER BY host;"
+
+    #     cur.execute(sql)
+
+
+    #     filter = [
+    #         {
+    #             row[0]: {
+    #                 "date": row[10],
+    #                 "host_name": row[1],
+    #                 "mac_address": row[2],
+    #                 "mac_vendor": row[3],
+    #                 "os_accuracy": row[5],
+    #                 "os_cpe": row[6],
+    #                 "os_name": row[4],
+    #                 "port_closed": json.loads(row[8]) if row[8] else [],
+    #                 "port_filtered": json.loads(row[9]) if row[9] else [],
+    #                 "port_open": json.loads(row[7]) if row[7] else [],
+    #                 "notation":row[11]
+    #             }
+    #         }
+    #         for row in cur.fetchall()
+            
+    #     ]
+    #     con.close()
+
+    #     return filter
+
+    # def summary():
+    #     con = sqlite3.connect(os.path.join(py_path, "database.db"))
+    #     cur = con.cursor()
+    #     sql = "SELECT * FROM data ORDER BY host;"
+
+    #     cur.execute(sql)
+
+    #     grouping = {}
+
+    #     for row in cur.fetchall():
+    #         ip = row[0]
+    #         octet_grouping = ".".join(ip.split(".")[:3]) + ".X"
+
+    #         if octet_grouping not in grouping: # if the octet grouping does not exist then add it as a new grouping
+    #             grouping[octet_grouping] = {
+    #                 "date":row[10],
+    #                 "notation":row[11],
+    #                 "total": 0
+    #             }
+
+    #         grouping[octet_grouping]["total"] +=1 # this increments total IPs with each loop
+    #     con.close()
+
+    #     result = [
+    #     {
+    #         ip_group: {
+    #             "date": data["date"],
+    #             "notation": data["notation"],
+    #             "total": data["total"]
+    #         }
+    #     }
+    #     for ip_group, data in grouping.items()
+    # ] 
+    #     return (result)
+   
+
+    # def save(host, host_name, mac, vendor, os_name, accuracy, cpe,  p_open, p_closed, p_filtered, now, notation):
+    #     con = sqlite3.connect(os.path.join(py_path, "database.db"))
+    #     cur = con.cursor()
+    #     try:
+    #         sql ='''
+    #         REPLACE INTO "data"
+    #         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)
+    #         '''
+
+    #         p_open_str = json.dumps(p_open)
+    #         p_closed_str = json.dumps(p_closed)
+    #         p_filtered_str = json.dumps(p_filtered)
+
+    #         values = [
+    #         host,
+    #         host_name,
+    #         mac, 
+    #         vendor, 
+    #         os_name, 
+    #         accuracy, 
+    #         cpe, 
+    #         p_open_str,
+    #         p_closed_str,
+    #         p_filtered_str,
+    #         now,
+    #         notation
+
+    #         ]
+    #         # print("values: ",values)
+
+    #     except Exception as e:
+    #         print(f"""database Error: {e}""")
+    #         pass
+
+        # cur.execute(sql, values)
+        # con.commit()
+        # con.close()
+
+Database.initial()
