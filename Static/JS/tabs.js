d6cd527438a2b0febc94bd25c953e6df8d7fdf17
SpaceWaffle42 <86594101+SpaceWaffle42@users.noreply.github.com>
2025-06-12 20:17:09 +0100

    Formatting for mobile

--- Static/JS/tabs.js
+++ Static/JS/tabs.js
@@ -1,4 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var tabs = document.querySelectorAll('.tabs');
-    M.Tabs.init(tabs, { swipeable: true });
+document.addEventListener('DOMContentLoaded', function () {
+  const tabElements = document.querySelectorAll('.tabs');
+  const instances = M.Tabs.init(tabElements, { swipeable: true });
+
+  requestAnimationFrame(() => {
+    tabElements.forEach((tabEl, index) => {
+      const tabs = tabEl.querySelectorAll('.tab');
+      const visibleTabLink = Array.from(tabs)
+        .find(tab => {
+          return (
+            !tab.classList.contains('desktop-only') ||
+            getComputedStyle(tab).display !== 'none'
+          );
+        })?.querySelector('a');
+
+      const activeTab = tabEl.querySelector('.tab a.active');
+
+      if (
+        activeTab &&
+        activeTab.parentElement.classList.contains('desktop-only') &&
+        getComputedStyle(activeTab.parentElement).display === 'none'
+      ) {
+        if (visibleTabLink) {
+          const tabId = visibleTabLink.getAttribute('href').substring(1);
+          instances[index].select(tabId);
+        }
+      }
+    });
   });
+});
